5e15eae4abd4abf2a9872f7e95226b530e2ae319
Mehmet Ali Karadağ <xxmehmetali@hotmail.com>
2024-01-19 21:58:39 +0300

    initial commit

---
+++ src/main/java/com/tv/tvserieswebapp/TvSeriesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tv.tvserieswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvSeriesWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvSeriesWebAppApplication.class, args);
+    }
+
+}
